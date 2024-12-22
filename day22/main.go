8273d5e672cc48edef1e9fbf560ad2bca610cca9
jonathan rengius <jonathan.rengius@outlook.com>
2024-12-22 16:31:59 +0100

    day22p2

--- day22/main.go
+++ day22/main.go
@@ -22,6 +22,10 @@ func main() {
 	partOne(numbers)
 	elapsed := time.Since(start)
 	fmt.Printf("Part one %s\n", elapsed)
+	start = time.Now()
+	partTwo(numbers)
+	elapsed = time.Since(start)
+	fmt.Printf("Part two %s\n", elapsed)
 
 }
 
@@ -36,11 +40,50 @@ func partOne(numbers []int) {
 	fmt.Println(sum)
 }
 
+func partTwo(numbers []int) {
+	bananaMap := getBananaMap(numbers)
+
+	result := 0
+	for _, total := range bananaMap {
+		if total > result {
+			result = total
+		}
+	}
+
+	fmt.Println(result)
+}
+
 func getSecretNumber(number int) int {
 	number = (number*64 ^ number) % 16777216
 	number = ((number/32 ^ number) % 16777216)
 	number = ((number*2048 ^ number) % 16777216)
 	return number
+}
+
+func getBananaMap(numbers []int) map[[4]int]int {
+	bananaMap := map[[4]int]int{}
+
+	for _, number := range numbers {
+		seen := make(map[[4]int]bool)
+		diffWindow := [4]int{10, 10, 10, 10}
+
+		for range 2000 {
+			before := number % 10
+			number = getSecretNumber(number)
+			current := number % 10
+
+			diffWindow[0] = diffWindow[1]
+			diffWindow[1] = diffWindow[2]
+			diffWindow[2] = diffWindow[3]
+			diffWindow[3] = current - before
+
+			if !seen[diffWindow] {
+				seen[diffWindow] = true
+				bananaMap[diffWindow] += current
+			}
+		}
+	}
+	return bananaMap
 }
 
 func parseInput(file *os.File) []int {
